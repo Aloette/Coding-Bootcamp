711d77cfd2ddd0e40ac79c884b1fae407872020c
Aloe <aloeanto@gmail.com>
2016-10-18 11:57:01 +0300

    Java exercises

--- Java/Constructing and using objects/Exercise_3.java
+++ Java/Constructing and using objects/Exercise_3.java
@@ -8,12 +8,13 @@ public class Exercise_3 {
 	    Scanner reader = new Scanner(System.in);
 	    System.out.println("Enter a number for the times you wish to shift the array elements: ");
 	    int num = reader.nextInt();
-	    System.out.println(Arrays.toString(Shift(pinakas,num)));
+	   // System.out.println(Arrays.toString(Shift(pinakas,num)));
+	    Shift(pinakas,num);
 	}
 
 	public static int[] Shift(int[] x, int n){	// method (function) that can shift the array elements right by 10 times
 	    int[] k = new int[10];
-	    for (int j=0; j<=n; j++){
+	    for (int j=1; j<=n; j++){
 			for (int i=0; i<x.length; i++){
 				if (i+1>=10){
 					k[i+1-10]=x[i];
@@ -22,9 +23,10 @@ public class Exercise_3 {
 					    k[i+1]=x[i];
 					}
 			}
-					for (int i=0; i<x.length; i++) {
+			for (int i=0; i<x.length; i++) {
 					    x[i]=k[i];
-					}
+			}
+			System.out.println(Arrays.toString(x));
 			}
 
 			return k;
